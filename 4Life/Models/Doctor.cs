e8b92b4f56dbbf7b679304d2b85cccd93898a9cc
bolog_sergiu <serjr2004@gmail.com>
2026-04-16 11:01:22 +0300

    recovered missing project files

---
+++ 4Life/Models/Doctor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Life.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public string? Specialization { get; set; }
+        public string? MedicalId { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public List<Medicine> PrescribedMedicines { get; set; } = new();
+    }
+}
